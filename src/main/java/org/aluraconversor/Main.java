722d0915830165be1099ac7a3109b4b064b6da9c
KemmerDesign <artset.kemmer@gmail.com>
2023-07-03 21:56:52 -0500

    Gui implementacion
    Se crean las dos ventanas principales para el reto, falta implementar la logica del programa.

---
+++ src/main/java/org/aluraconversor/Main.java
@@ -0,0 +1,17 @@
+package org.aluraconversor;
+
+import org.aluraconversor.ui.MenuEleccion;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        Object opciones[] = {"Conversor de moneda", "Conversor de temperatura"};
+        MenuEleccion menu = new MenuEleccion(opciones,"Escoja lo que desea hacer","Conversor Alura");
+
+
+    }
+}
